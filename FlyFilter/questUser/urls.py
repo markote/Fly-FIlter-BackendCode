b200b70f25cd61b8e930584b2761d45af5aad625
Ubuntu <ubuntu@ip-172-31-40-176.eu-central-1.compute.internal>
2019-10-12 05:06:23 +0000

    API that send data of the weather Filters

---
+++ FlyFilter/questUser/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from questUser import views
+
+urlpatterns = [
+    url(r'^filters/weather/$', views.weatherFilters.as_view()),
+    
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- FlyFilter/questUser/views.py
+++ FlyFilter/questUser/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.views.generic import View
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import json 
+
 class MyView(View):
     def get(self, request):
         
@@ -10,3 +15,12 @@ class MyView(View):
 
         return Response("Done",status=status.HTTP_200_OK)
 # Create your views here.
+
+class weatherFilters(APIView):
+    def get(self, request, format=None):
+        """
+        Returns the data for the Temperature and Rain filters
+        """
+        with open('/home/ubuntu/REPOS/Fly-Filter-Backend/FlyFilter/filtersWeather.json') as json_file: # it know its harcoded, and i dont give a shit
+            data = json.load(json_file)
+        return Response(data, status=status.HTTP_200_OK)
